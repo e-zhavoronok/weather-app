73219b33384cad8f52a58df1692059d3366bd96f
Evgeniy Zhavoronok <evgeniyzhavoronok@gmail.com>
2022-02-11 13:32:06 +0300

    add server and client

---
+++ src/app.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const geocode = require('../utils/geocode');
+const forecast = require('../utils/forecast');
+
+const app = express();
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup public directory
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather Forecast'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide a search term"
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longtitude, location } = {}) => {
+        if (error) {
+            return res.send({ error });
+        }
+
+        forecast(latitude, longtitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error });
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            });
+        });
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helptext: "A helpful text",
+        title: 'Help'
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: "404",
+        errorText: "Help article not found"
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: "404",
+        errorText: "Page not found"
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+});
